4900534130e7f5c0abd97c1c7d573832b8b4b0f2
David Aradom <davidaradom@yahoo.com>
2019-11-16 21:19:20 -0600

    final update

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,14 +1,14 @@
 
 // Set up our chart
 //= ================================
-var svgWidth = 960;
-var svgHeight = 500;
+var svgWidth = 900;
+var svgHeight = 600;
 
 var margin = {
-  top: 20,
+  top: 40,
   right: 40,
-  bottom: 60,
-  left: 50
+  bottom: 80,
+  left: 90
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -18,58 +18,110 @@ var height = svgHeight - margin.top - margin.bottom;
 // and shift the latter by left and top margins.
 // =================================
 var svg = d3
-  .select("#scatter")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+.select("#scatter")
+.append("svg")
+.attr("width", svgWidth)
+.attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Import data from the data.csv file
-// =================================
-d3.csv("data.csv").then(function(censusData) {
-    // Step 4: Parse the data
-    // Format the data and convert to numerical and date values
-    // =================================
-    // Create a function to parse date and time
-    
+// Set variable for csv and read file
+var file = "assets/data/data.csv"
+
+d3.csv(file).then(successHandle, errorHandle);
+
+function errorHandle(error) {
+  throw err;
+}
+
+// Set up data for chart
+
+function successHandle(censusData) {
+  censusData.map(function (data) {
+    data.poverty = +data.poverty;
+    data.obesity = +data.obesity;
+  });
+  var xLinearScale = d3.scaleLinear()
+  .domain([8.1, d3.max(censusData, d => d.poverty)])
+  .range([0, width]);
+ 
+  var yLinearScale = d3.scaleLinear()
+  .domain([20, d3.max(censusData, d => d.obesity)])
+  .range([height, 0]);
+ 
+  var bottomAxis = d3.axisBottom(xLinearScale)
+  .ticks(7);
   
-    // Format the data
-    censusData.forEach(function(data) {
-      data.healthcare = +data.healthcare;
-      data.poverty = +data.poverty;
-      data.smokes = +data.smokes;
-      data.age = +data.age;
-      data.obesity = +data.obesity;
-      data.income = +data.income;
-      data.abbr = +data.abbr;
-    });
-    
-    // Step 5: Create the scales for the chart
-    // =================================
-    // var xTimeScale = d3.scaleTime()
-    // .domain(d3.extent(censusData, d => d.poverty))
-    // .range([0, width]);
-
-    var xLinearScale = d3.scaleLinear().range([0, width]);
-    
-    var yLinearScale = d3.scaleLinear().range([height, 0]);
-    
-    // Step 6: Set up the y-axis domain
-    // ==============================================
-    // @NEW! determine the max y value
-    // find the max of the morning data
-    var morningMax = d3.max(donutData, d => d.morning);
-    // find the max of the evening data
-    var eveningMax = d3.max(donutData, d => d.evening);
-    var yMax;
-    if (morningMax > eveningMax) {
-        yMax = morningMax;
-    }
-    else {
-        yMax = eveningMax;
-    }
-    // var yMax = morningMax > eveningMax ? morningMax : eveningMax;
-    // Use the yMax value to set the yLinearScale domain
-    yLinearScale.domain([0, yMax]);
+  var leftAxis = d3.axisLeft(yLinearScale);
+ 
+  chartGroup.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(bottomAxis);
+ 
+  chartGroup.append("g")
+  .call(leftAxis);
+ 
+  // circles 
+ 
+  var circlesGroup = chartGroup.selectAll("circle")
+  .data(censusData)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xLinearScale(d.poverty))
+  .attr("cy", d => yLinearScale(d.obesity))
+  .attr("r", "15")
+  .attr("fill", "blue")
+  .attr("opacity", ".5")
+ 
+ 
+  // circle labels
+  var circlesGroup = chartGroup.selectAll()
+  .data(censusData)
+  .enter()
+  .append("text")
+  .attr("x", d => xLinearScale(d.poverty))
+  .attr("y", d => yLinearScale(d.obesity))
+  .style("font-size", "12px")
+  .style("text-anchor", "middle")
+  .style('fill', 'white')
+  .text(d => (d.abbr));
+ 
+  // tooltip
+ 
+  var toolTip = d3.tip()
+  .attr("class", "tooltip")
+  .offset([80, -60])
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "3px")
+  .style("border-radius", "5px")
+  .style("padding", "5px")
+  .html(function (d) {
+    return (`${d.state}<br>Poverty: ${d.poverty}%<br>Obesity: ${d.obesity}% `);
+  });
+ 
+  chartGroup.call(toolTip);
+ 
+  circlesGroup.on("mouseover", function (data) {
+    toolTip.show(data, this);
+  })
+  .on("mouseout", function (data) {
+    toolTip.hide(data);
+  });
+ 
+  // Y Axis
+  chartGroup.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left + 40)
+  .attr("x", 0 - (height / 2))
+  .attr("dy", "1em")
+  .attr("class", "axisText")
+  .text("Obese (%)");
+ 
+  // X Axis
+  chartGroup.append("text")
+  .attr("transform", `translate(${width / 2}, ${height + margin.top + 5})`)
+  .attr("class", "axisText")
+  .text("In Poverty (%)");
+}
